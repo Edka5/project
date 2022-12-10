48fd4e7dc47dd3b308747e73947f9a3177b6d351
Edka5 <118814110+Edka5@users.noreply.github.com>
2022-12-11 00:32:21 +0900

    Add files via upload

---
+++ task4/script.js
@@ -0,0 +1,8 @@
+var newText = document.querySelector(".day");
+function text()
+ {
+    var textInfo = prompt("Введите текст");
+    newText.textContent = textInfo;
+    
+}
+newText.addEventListener('click', text);
